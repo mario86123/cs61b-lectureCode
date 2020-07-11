98817c4a2e577d0a282661cbebf01843ed35dd0b
mario86123 <mario86123.eed05@g2.nctu.edu.tw>
2020-07-11 14:14:48 +0800

    implement size, iterativeSize, get method

--- exercises/lists1/IntList.java
+++ exercises/lists1/IntList.java
@@ -9,24 +9,42 @@ public class IntList {
 
 	/** Return the size of the list using... recursion! */
 	public int size() {
-		return 0;
+		if (rest == null) {
+			return 1;
+		} else {
+			return rest.size() + 1;
+		}
 	}
 
 	/** Return the size of the list using no recursion! */
 	public int iterativeSize() {
-		return 0;
+		IntList ptr = this;
+		int size = 0;
+		while (ptr != null) {
+			size += 1;
+			ptr = ptr.rest;
+		}
+		return size;
 	}
 
 	/** Returns the ith value in this list.*/
 	public int get(int i) {
-		return 0;
+		IntList ptr = this;
+		for (int k = 0; k < i; k += 1) {
+			ptr = ptr.rest;
+		}
+		return ptr.first;
 	}
 
 	public static void main(String[] args) {
 		IntList L = new IntList(15, null);
 		L = new IntList(10, L);
 		L = new IntList(5, L);
-
+		System.out.println(L.size());
 		System.out.println(L.iterativeSize());
+		System.out.println(L.rest.size());
+		System.out.println(L.rest.iterativeSize());
+		System.out.println(L.get(0));
+		System.out.println(L.get(1));
 	}
 } 
